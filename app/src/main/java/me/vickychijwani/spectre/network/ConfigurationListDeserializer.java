529c6741a0d91aa36d918502c5cdcdf50c63ca78
Vicky Chijwani <vickychijwani@gmail.com>
2019-04-03 00:14:06 +0530

    🐛 Additional bug fix for #38: Allow nulls in ConfigurationParam values

--- app/src/main/java/me/vickychijwani/spectre/network/ConfigurationListDeserializer.java
+++ app/src/main/java/me/vickychijwani/spectre/network/ConfigurationListDeserializer.java
@@ -46,13 +46,12 @@ import me.vickychijwani.spectre.util.log.Log;
     }
 
     private ConfigurationParam makeConfigParam(String key, JsonElement value) {
-        if (value.isJsonNull()) {
-            throw new NullPointerException("value for key '" + key + "' is null!");
-        }
         ConfigurationParam param = new ConfigurationParam();
         param.setKey(key);
         String valueStr;
-        if (value.isJsonPrimitive() && value.getAsJsonPrimitive().isString()) {
+        if (value.isJsonNull()) {
+            valueStr = "";
+        } else if (value.isJsonPrimitive() && value.getAsJsonPrimitive().isString()) {
             // toString would return the string with quotes around it which we don't want
             valueStr = value.getAsString();
         } else {
